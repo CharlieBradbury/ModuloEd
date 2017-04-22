f71d9c656b614a5f64db8b4e64a3892261d006e7
Edgar Bustillos <edgar_daniel_1996@hotmail.com>
2017-04-22 16:56:57 -0500

    Base project

---
+++ EyAP_ProyectoFinal/src/eyap_proyectofinal/FXMLVistaTema1Controller.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package eyap_proyectofinal;
+
+import com.jfoenix.controls.JFXButton;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+
+/**
+ * FXML Controller class
+ *
+ * @author edgar
+ */
+public class FXMLVistaTema1Controller implements Initializable {
+
+    /**
+     * Initializes the controller class.
+     */
+    
+     @FXML
+    private JFXButton btnInicio;
+     
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+}
